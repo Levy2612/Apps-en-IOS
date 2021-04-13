1fac8627ac572a177c053d85c1632af62405c605
Levy2612 <78839541+Levy2612@users.noreply.github.com>
2021-04-12 17:11:50 -0700

    Add files via upload

---
+++ Actividad10.1_LAAS/Actividad10.1_LAASTests/Actividad10_1_LAASTests.swift
@@ -0,0 +1,34 @@
+//
+//  Actividad10_1_LAASTests.swift
+//  Actividad10.1_LAASTests
+//
+//  Created by user190575 on 4/12/21.
+//  Copyright © 2021 user190575. All rights reserved.
+//
+
+import XCTest
+@testable import Actividad10_1_LAAS
+
+class Actividad10_1_LAASTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
