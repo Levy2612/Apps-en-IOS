7293fab71666316a806d0a572c11fb89da8f64ad
Levy2612 <78839541+Levy2612@users.noreply.github.com>
2021-11-03 15:50:41 -0600

    Add files via upload

---
+++ BudaDegollado/BudaDegollado/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BudaDegollado
+//
+//  Created by Alumno on 03/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ BudaDegollado/BudaDegolladoUITests/BudaDegolladoUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  BudaDegolladoUITestsLaunchTests.swift
+//  BudaDegolladoUITests
+//
+//  Created by Alumno on 03/11/21.
+//
+
+import XCTest
+
+class BudaDegolladoUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
